4fe2011ec17933183e36407c27212d17a68eb6c9
costdev <costdev@git.wordpress.org>
2025-02-04 22:24:39 +0000

    Tests: Create admin and editor users for AdminSettings tests.

--- tests/phpunit/includes/AdminSettings_UnitTestCase.php
+++ tests/phpunit/includes/AdminSettings_UnitTestCase.php
@@ -6,6 +6,20 @@
  */
 abstract class AdminSettings_UnitTestCase extends WP_UnitTestCase {
 	/**
+	 * The user ID of an administrator.
+	 *
+	 * @var int
+	 */
+	protected static $admin_id;
+
+	/**
+	 * The user ID of an editor.
+	 *
+	 * @var int
+	 */
+	protected static $editor_id;
+
+	/**
 	 * The Name of the Option.
 	 *
 	 * @var string
@@ -20,7 +34,19 @@ abstract class AdminSettings_UnitTestCase extends WP_UnitTestCase {
 	protected static $options_page = 'aspireupdate-settings';
 
 	/**
-	 * Deletes settings before each test runs.
+	 * Creates administrator and editor users before any tests run.
+	 *
+	 * @return void
+	 */
+	public static function set_up_before_class() {
+		parent::set_up_before_class();
+
+		self::$admin_id  = self::factory()->user->create( [ 'role' => 'administrator' ] );
+		self::$editor_id = self::factory()->user->create( [ 'role' => 'editor' ] );
+	}
+
+	/**
+	 * Deletes settings and sets the current user before each test runs.
 	 *
 	 * @return void
 	 */
@@ -28,5 +54,6 @@ abstract class AdminSettings_UnitTestCase extends WP_UnitTestCase {
 		parent::set_up();
 
 		delete_site_option( self::$option_name );
+		wp_set_current_user( self::$admin_id );
 	}
 }
