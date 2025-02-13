0940aee6ac9d134d73498dfd1cc92c45ae01a723
costdev <costdev@git.wordpress.org>
2025-02-13 02:12:08 +0000

    E2E Tests: Add field tests for `disable_ssl`.

---
+++ tests/e2e/admin-settings/disable_ssl.spec.ts
@@ -0,0 +1,26 @@
+import { expect, test } from '@playwright/test';
+import { fieldSelector, enable_debug, disable_ssl } from '../data/fields';
+import { settings } from '../data/routes';
+
+test.describe('[Admin Settings] [Field] Disable SSL Verification', () => {
+	test.beforeEach(async ( { page }) => {
+		const baseURL = test.info().project.use.baseURL;
+		const settingsPage = `${baseURL}/wp-admin/${settings}`;
+		await page.goto(settingsPage);
+		await page.waitForURL(settingsPage);
+	});
+
+	test('is invisible by default', async ({ page }) => {
+		await expect(page.locator(fieldSelector + disable_ssl)).not.toBeVisible();
+	});
+
+	test('is visible when debugging is enabled', async ({ page }) => {
+		await page.check(fieldSelector + enable_debug);
+		await expect(page.locator(fieldSelector + disable_ssl)).toBeVisible();
+	});
+
+	test('is unchecked by default', async ({ page }) => {
+		await page.check(fieldSelector + enable_debug);
+		await expect(page.locator(fieldSelector + disable_ssl)).not.toBeChecked();
+	});
+})
