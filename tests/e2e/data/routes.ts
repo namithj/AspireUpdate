30e4a8232398044b059620f08eb5ae5d4e54308f
costdev <costdev@git.wordpress.org>
2025-02-13 02:09:07 +0000

    E2E Tests: Add fields and routes for convenience.

---
+++ tests/e2e/data/routes.ts
@@ -0,0 +1,6 @@
+
+export const settings = 'index.php?page=aspireupdate-settings';
+export const plugins = 'plugins.php';
+export const pluginInstall = 'plugin-install.php';
+export const themes = 'themes.php';
+export const themeInstall = 'theme-install.php';
