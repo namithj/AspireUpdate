32df1167dd90e296851d7e3409ce1a35cfa09448
costdev <costdev@git.wordpress.org>
2025-02-13 02:11:47 +0000

    E2E Tests: Add field tests for `debug_request`.

---
+++ tests/e2e/admin-settings/debug_request.spec.ts
@@ -0,0 +1,26 @@
+import { expect, test } from '@playwright/test';
+import { fieldSelector, enable_debug, debug_request } from '../data/fields';
+import { settings } from '../data/routes';
+
+test.describe('[Admin Settings] [Field] Debug Type: Request', () => {
+	test.beforeEach(async ( { page }) => {
+		const baseURL = test.info().project.use.baseURL;
+		const settingsPage = `${baseURL}/wp-admin/${settings}`;
+		await page.goto(settingsPage);
+		await page.waitForURL(settingsPage);
+	});
+
+	test('is invisible by default', async ({ page }) => {
+		await expect(page.locator(fieldSelector + debug_request)).not.toBeVisible();
+	});
+
+	test('is visible when debugging is enabled', async ({ page }) => {
+		await page.check(fieldSelector + enable_debug);
+		await expect(page.locator(fieldSelector + debug_request)).toBeVisible();
+	});
+
+	test('is unchecked by default', async ({ page }) => {
+		await page.check(fieldSelector + enable_debug);
+		await expect(page.locator(fieldSelector + debug_request)).not.toBeChecked();
+	});
+})
