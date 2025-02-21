bd3aa68fbe04c3a29cba4c9c6a432ecdeaf74030
costdev <costdev@git.wordpress.org>
2025-02-21 13:29:58 +0000

    Admin_Settings: Change `Clear Log`+`View Log` to `button` elements.
    Previously, these were `a` elements with `href="javascript:;"`, which produced redundant links warnings.

--- includes/views/page-admin-settings.php
+++ includes/views/page-admin-settings.php
@@ -15,8 +15,8 @@ $option_group = $args['option_group'] ?? '';
 			<?php wp_nonce_field( 'aspireupdate-settings' ); ?>
 			<?php submit_button( '', 'primary', 'submit', false ); ?>
 			<a href="<?php echo esc_url( $reset_url ); ?>" class="button button-secondary" ><?php esc_html_e( 'Reset', 'aspireupdate' ); ?></a>
-			<a href="javascript:;" id="aspireupdate-button-clearlog" class="button button-secondary button-clearlog" ><?php esc_html_e( 'Clear Log', 'aspireupdate' ); ?></a>
-			<a href="javascript:;" id="aspireupdate-button-viewlog" class="button button-secondary button-viewlog" ><?php esc_html_e( 'View Log', 'aspireupdate' ); ?></a>
+			<button type="button" id="aspireupdate-button-clearlog" class="button button-secondary button-clearlog" ><?php esc_html_e( 'Clear Log', 'aspireupdate' ); ?></button>
+			<button type="button" id="aspireupdate-button-viewlog" class="button button-secondary button-viewlog" ><?php esc_html_e( 'View Log', 'aspireupdate' ); ?></button>
 		</p>
 	</form>
 	<div id="aspireupdate-log-viewer">
