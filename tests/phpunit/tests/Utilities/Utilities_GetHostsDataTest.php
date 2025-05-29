f249f349203f32487abd943c86725e881bd76531
Namith Jawahar <namith.jawahar@gmail.com>
2025-05-30 00:52:59 +0530

    PHP Doc Comments Added in for Tests
    PHP Doc Comments Added in for Tests

--- tests/phpunit/tests/Utilities/Utilities_GetHostsDataTest.php
+++ tests/phpunit/tests/Utilities/Utilities_GetHostsDataTest.php
@@ -5,15 +5,45 @@
  * @package AspireUpdate
  */
 
-use AspireUpdate\Utilities;
 
+/**
+ * Tests for Utilities::get_hosts_data()
+ *
+ * These tests cause constants to be defined.
+ * They must run in separate processes and must not preserve global state.
+ *
+ * @runTestsInSeparateProcesses
+ * @preserveGlobalState disabled
+ *
+ * @covers \AspireUpdate\Utilities::get_hosts_data
+ */
 class Utilities_GetHostsDataTest extends \WP_UnitTestCase {
+	/**
+	 * Test that an array is returned by get_hosts_data().
+	 *
+	 * @dataProvider data_multisite_hooks_and_methods
+	 *
+	 * @group ms-required
+	 *
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 */
 	public function test_get_hosts_data_returns_array() {
 		$hosts_data = \AspireUpdate\Utilities::get_hosts_data();
 		$this->assertTrue( is_array( $hosts_data ), 'get_hosts_data() should return an array' );
 		$this->assertNotEmpty( $hosts_data, 'get_hosts_data() should not return an empty array' );
 	}
 
+	/**
+	 * Test that the array elements returned by get_hosts_data() contained the required parameters.
+	 *
+	 * @dataProvider data_multisite_hooks_and_methods
+	 *
+	 * @group ms-required
+	 *
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 */
 	public function test_get_hosts_data_contains_expected_keys() {
 		$hosts_data = \AspireUpdate\Utilities::get_hosts_data();
 		$this->assertTrue( is_array( $hosts_data ) );
@@ -23,6 +53,16 @@ class Utilities_GetHostsDataTest extends \WP_UnitTestCase {
 		}
 	}
 
+	/**
+	 * Test that the get_hosts_data() returns false when the hosts.json file is missing.
+	 *
+	 * @dataProvider data_multisite_hooks_and_methods
+	 *
+	 * @group ms-required
+	 *
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 */
 	public function test_get_hosts_data_returns_false_on_missing_file() {
 		global $wp_filesystem;
 		$file   = defined( 'AP_PATH' ) ? AP_PATH . DIRECTORY_SEPARATOR . 'hosts.json' : null;
